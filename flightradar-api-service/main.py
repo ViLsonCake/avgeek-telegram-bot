9be7fb17f15da42898a8b925a9453857ba1e756c
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-05-26 16:50:00 +0200

    add exception handling

--- flightradar-api-service/main.py
+++ flightradar-api-service/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends
+from FlightRadar24.errors import AirportNotFoundError
+from fastapi import FastAPI, Depends, HTTPException, status
 from security.api_key_security import api_key_auth
 from FlightRadar24 import FlightRadar24API
 from utils.flight_utils import get_white_list_plane_codes
@@ -21,7 +22,19 @@ async def get_aircraft_by_icao(icao: str, api_key=Depends(api_key_auth)) -> dict
 
 @app.get('/airport/{icao}')
 async def get_white_list_planes(icao: str, api_key=Depends(api_key_auth)) -> dict:
-    details = flightradar_api.get_airport_details(icao)
+    try:
+        details = flightradar_api.get_airport_details(icao)
+    except AirportNotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Airport with code "{icao}" not found'
+        )
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'The code "{icao}" is not valid. It must be the IATA or ICAO of the airport'
+        )
+
     arrivals: list = details['airport']['pluginData']['schedule']['arrivals']['data']
     formatted_arrivals: list = []
 
