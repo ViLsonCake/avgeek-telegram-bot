8ab1e0e5d61435867c8c7b4192b87d6297d00395
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-06-08 03:09:28 +0200

    add commands to menu button

--- telegram-bot/src/main/java/project/vilsoncake/telegrambot/bot/AvgeekTelegramBot.java
+++ telegram-bot/src/main/java/project/vilsoncake/telegrambot/bot/AvgeekTelegramBot.java
@@ -2,13 +2,20 @@ package project.vilsoncake.telegrambot.bot;
 
 import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.menubutton.SetChatMenuButton;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import project.vilsoncake.telegrambot.entity.enumerated.UserState;
 import project.vilsoncake.telegrambot.property.BotProperties;
 import project.vilsoncake.telegrambot.service.BotService;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static project.vilsoncake.telegrambot.constant.CommandConst.*;
 
 @Component
 public class AvgeekTelegramBot extends AbilityBot {
@@ -23,6 +30,22 @@ public class AvgeekTelegramBot extends AbilityBot {
     @Override
     public long creatorId() {
         return 1L;
+    }
+
+    @Override
+    public void onRegister() {
+        List<BotCommand> botCommands = new ArrayList<>();
+        botCommands.add(new BotCommand(START_COMMAND_NAME, START_COMMAND_DESCRIPTION));
+        botCommands.add(new BotCommand(PING_COMMAND_NAME, PING_COMMAND_DESCRIPTION));
+        botCommands.add(new BotCommand(CHANGE_AIRPORT_COMMAND_NAME, CHANGE_AIRPORT_COMMAND_DESCRIPTION));
+        botCommands.add(new BotCommand(CURRENT_AIRPORT_COMMAND_NAME, CURRENT_AIRPORT_COMMAND_DESCRIPTION));
+
+        try {
+            execute(new SetMyCommands(botCommands, new BotCommandScopeDefault(), null));
+            execute(new SetChatMenuButton());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
---
+++ telegram-bot/src/main/java/project/vilsoncake/telegrambot/constant/CommandConst.java
@@ -0,0 +1,12 @@
+package project.vilsoncake.telegrambot.constant;
+
+public class CommandConst {
+    public static final String START_COMMAND_NAME = "/start";
+    public static final String START_COMMAND_DESCRIPTION = "Start the bot and select an airport";
+    public static final String CHANGE_AIRPORT_COMMAND_NAME = "/changeairport";
+    public static final String CHANGE_AIRPORT_COMMAND_DESCRIPTION = "Change your airport";
+    public static final String CURRENT_AIRPORT_COMMAND_NAME = "/currentairport";
+    public static final String CURRENT_AIRPORT_COMMAND_DESCRIPTION = "Get your current airport";
+    public static final String PING_COMMAND_NAME = "/ping";
+    public static final String PING_COMMAND_DESCRIPTION = "Check bot connection";
+}
