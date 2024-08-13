9ef9051a8a5e7f57fd7f8f90fd5f629d9b488fe7
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-08-13 03:19:21 +0200

    added new mapping and refactoring

---
+++ flightradar-api-service/dto/flight_dto.py
@@ -0,0 +1,66 @@
+import FlightRadar24
+
+from utils.flight_utils import convert_knots_to_kmh, convert_feet_to_meters, get_aircraft_name_by_code
+
+
+class Flight:
+    id: str
+    code: str
+    callsign: str
+    altitude: int
+    ground_speed: int
+    vertical_speed: int
+    latitude: float
+    longitude: float
+    distance: int
+
+    def __init__(self, flight: FlightRadar24.Flight, airport: FlightRadar24.Airport):
+        self.id = flight.id
+        self.code = flight.aircraft_code
+        self.callsign = flight.callsign
+        self.altitude = convert_feet_to_meters(flight.altitude)
+        self.ground_speed = convert_knots_to_kmh(flight.ground_speed)
+        self.vertical_speed = flight.vertical_speed
+        self.latitude = flight.latitude
+        self.longitude = flight.longitude
+        self.distance = int(flight.get_distance_from(airport))
+
+
+class ScheduledFlight:
+    id: str
+    code: str
+    aircraft: str
+    airline_name: str
+    airport: str
+    iata: str
+    icao: str
+    callsign: str
+    registration: str
+    live: bool
+    text: str
+
+    def __init__(self, row_flight: dict):
+        self.id: str = row_flight['flight']['identification']['id']
+
+        if self.id is None:
+            self.id = str(row_flight['flight']['identification']['row'])
+
+        self.airport: str = row_flight['flight']['airport']['origin']['name']
+        self.iata: str = row_flight['flight']['airport']['origin']['code']['iata']
+        self.icao: str = row_flight['flight']['airport']['origin']['code']['icao']
+        self.callsign: str = row_flight['flight']['identification']['callsign']
+        self.registration: str = row_flight['flight']['aircraft']['registration']
+        self.live: bool = row_flight['flight']['status']['live']
+        self.text: str = row_flight['flight']['status']['text']
+
+        try:
+            self.code: str = row_flight['flight']['aircraft']['model']['code']
+            self.aircraft = get_aircraft_name_by_code(self.code)
+        except TypeError:
+            self.code = 'Unknown'
+            self.aircraft = 'Unknown'
+
+        try:
+            self.airline_name: str = row_flight['flight']['airline']['name']
+        except TypeError:
+            self.airline_name = 'Unknown'
--- flightradar-api-service/main.py
+++ flightradar-api-service/main.py
@@ -2,14 +2,16 @@ from FlightRadar24.errors import AirportNotFoundError
 from fastapi import FastAPI, Depends, HTTPException, status
 from security.api_key_security import api_key_auth
 from FlightRadar24 import FlightRadar24API
-from utils.flight_utils import get_white_list_plane_codes, get_aircraft_name_by_code, convert_feet_to_meters, convert_knots_to_kmh
+from utils.flight_utils import get_white_list_plane_codes
+
+from dto.flight_dto import Flight, ScheduledFlight
 
 app = FastAPI()
 flightradar_api: FlightRadar24API = FlightRadar24API()
 
 
 @app.get('/ping')
-async def pong(api_key=Depends(api_key_auth)) -> str:
+async def pong(api_key=Depends(api_key_auth)):
     return 'pong'
 
 
@@ -17,18 +19,24 @@ async def pong(api_key=Depends(api_key_auth)) -> str:
 async def get_aircraft_by_code(aircraft_code: str, airport_code: str, api_key=Depends(api_key_auth)):
     flights: list = flightradar_api.get_flights(aircraft_type=aircraft_code)
     airport = flightradar_api.get_airport(airport_code)
-    flights = [{
-        'id': flight.id,
-        'code': flight.aircraft_code,
-        'callsign': flight.callsign,
-        'altitude': convert_feet_to_meters(flight.altitude),
-        'ground_speed': convert_knots_to_kmh(flight.ground_speed),
-        'vertical_speed': flight.vertical_speed,
-        'latitude': flight.latitude,
-        'longitude': flight.longitude,
-        'distance': int(flight.get_distance_from(airport))
-    } for flight in flights]
+    flights = [Flight(flight, airport) for flight in flights]
     return {'flights': flights}
+
+
+@app.get('/registration/{registration}/{airport_code}')
+async def get_flight_by_registration(registration: str, airport_code: str, api_key=Depends(api_key_auth)):
+    flights = flightradar_api.get_flights(registration=registration)
+
+    if not flights:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Flight with registration {registration} not found'
+        )
+
+    flight = flights[0]
+    airport = flightradar_api.get_airport(airport_code)
+
+    return Flight(flight, airport)
 
 
 @app.get('/airport/flights/{code}')
@@ -38,54 +46,16 @@ async def get_white_list_planes(code: str, api_key=Depends(api_key_auth)):
     except AirportNotFoundError:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f'Airport with code "{code}" not found'
+            detail=f'Airport with code {code} not found'
         )
     except ValueError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f'The code "{code}" is not valid. It must be the IATA or ICAO of the airport'
+            detail=f'The code {code} is not valid. It must be the IATA or ICAO of the airport'
         )
 
     arrivals: list = details['airport']['pluginData']['schedule']['arrivals']['data']
     row_white_list_arrivals: list = get_white_list_plane_codes(arrivals)
-    formatted_white_list_arrivals: list = []
 
-    for arrival in row_white_list_arrivals:
-        id: str = arrival['flight']['identification']['id']
+    return {'flights': [ScheduledFlight(flight) for flight in row_white_list_arrivals]}
 
-        if id is None:
-            id = str(arrival['flight']['identification']['row'])
-
-        airport: str = arrival['flight']['airport']['origin']['name']
-        iata: str = arrival['flight']['airport']['origin']['code']['iata']
-        icao: str = arrival['flight']['airport']['origin']['code']['icao']
-        callsign: str = arrival['flight']['identification']['callsign']
-        registration: str = arrival['flight']['aircraft']['registration']
-        live: bool = arrival['flight']['status']['live']
-        text: str = arrival['flight']['status']['text']
-
-        try:
-            code: str = arrival['flight']['aircraft']['model']['code']
-        except TypeError:
-            code = 'Unknown'
-
-        try:
-            airline_name: str = arrival['flight']['airline']['name']
-        except TypeError:
-            airline_name = 'Unknown'
-
-        formatted_white_list_arrivals.append({
-            'id': id,
-            'code': code,
-            'aircraft': get_aircraft_name_by_code(code),
-            'airline_name': airline_name,
-            'airport': airport,
-            'iata': iata,
-            'icao': icao,
-            'callsign': callsign,
-            'registration': registration,
-            'live': live,
-            'text': text
-        })
-
-    return {'flights': formatted_white_list_arrivals}
