099e210691401d89184da3cdd117db3d7cdac23a
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-08-24 20:52:22 +0200

    refactoring

--- telegram-bot/src/main/java/project/vilsoncake/telegrambot/constant/NumberConst.java
+++ telegram-bot/src/main/java/project/vilsoncake/telegrambot/constant/NumberConst.java
@@ -3,7 +3,7 @@ package project.vilsoncake.telegrambot.constant;
 public class NumberConst {
     public static final int SCHEDULED_FLIGHTS_CHECK_DELAY_IN_MINUTES = 10;
     public static final int LANDING_FLIGHTS_CHECK_DELAY_IN_MINUTES = 7;
-    public static final int AN_124_FLIGHTS_CHECK_DELAY_IN_MINUTES = 5;
+    public static final int AN_124_FLIGHTS_CHECK_DELAY_IN_MINUTES = 2;
     public static final int FLIGHT_CLOSE_TO_AIRPORT_DISTANCE_IN_KM = 175;
     public static final int FLIGHT_IN_AIRPORT_DISTANCE_IN_KM = 15;
     public static final int FLIGHT_MAYBE_IN_AIRPORT_ALTITUDE_IN_M = 6000;
