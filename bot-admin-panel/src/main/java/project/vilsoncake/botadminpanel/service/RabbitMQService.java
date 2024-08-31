7ea20a5ccc5ab74d6c8087d9a15e6a89de7480b7
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-08-31 21:55:36 +0200

    refactoring

--- bot-admin-panel/src/main/java/project/vilsoncake/botadminpanel/service/RabbitMQService.java
+++
@@ -1,9 +0,0 @@
-package project.vilsoncake.botadminpanel.service;
-
-import project.vilsoncake.botadminpanel.dto.MessageDto;
-import project.vilsoncake.botadminpanel.dto.UserStatisticDto;
-
-public interface RabbitMQService {
-    void consumeUserStatisticMessage(UserStatisticDto userStatisticDto);
-    boolean sendMessage(MessageDto messageDto);
-}
