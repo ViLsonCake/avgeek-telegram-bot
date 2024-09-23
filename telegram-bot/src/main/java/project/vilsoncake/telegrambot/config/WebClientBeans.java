5ad0654b56db1741817d705daa02a5b26ac6ddea
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-09-23 20:46:42 +0200

    added timeout to api webclient

--- telegram-bot/src/main/java/project/vilsoncake/telegrambot/config/WebClientBeans.java
+++ telegram-bot/src/main/java/project/vilsoncake/telegrambot/config/WebClientBeans.java
@@ -1,11 +1,18 @@
 package project.vilsoncake.telegrambot.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import project.vilsoncake.telegrambot.property.FlightradarApiProperties;
 import project.vilsoncake.telegrambot.property.GeonamesProperties;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,6 +25,12 @@ public class WebClientBeans {
     public WebClient apiWebClient() {
         return WebClient.builder()
                 .defaultHeader("x-api-key", flightradarApiProperties.getApiKey())
+                .defaultHeader(HttpHeaders.AUTHORIZATION, MediaType.APPLICATION_JSON_VALUE)
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                        .doOnConnected(connection -> connection
+                                .addHandlerLast(new ReadTimeoutHandler(5))
+                                .addHandlerLast(new WriteTimeoutHandler(5)))))
                 .baseUrl(flightradarApiProperties.getUrl())
                 .build();
     }
