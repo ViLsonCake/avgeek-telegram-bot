b94c342cd46d5aeb93e7f225b45488378d2cf208
Tymofii Dmytrenko <timofeydmitrenko13@gmail.com>
2025-06-29 21:23:00 +0200

    Fixed Flightradar API issue

--- flightradar-api-service/utils/flight_utils.py
+++ flightradar-api-service/utils/flight_utils.py
@@ -1,32 +1,69 @@
 import os
 import json
 
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-def get_json_config() -> dict[str, str]:
-    with open(os.getcwd() + '/config/whitelist-aircraft-codes.json', 'r') as config_file:
-        string_json = config_file.read()
+class AircraftUtils(metaclass=Singleton):
 
-    return json.loads(string_json)['white_list_codes']
+    __data: dict[str, str]
+
+    def __init__(self):
+        self.__data = self.__load_json_data()
+
+    def __load_json_data(self) -> dict[str, str]:
+        with open(os.getcwd() + '/config/whitelist-aircraft-codes.json', 'r') as config_file:
+            string_json = config_file.read()
+
+        return json.loads(string_json)
+
+    def get_data(self):
+        return self.__data
+
+    def get_aircraft_name_by_code(self, code: str) -> str:
+        return self.get_data()[code]
+
+    def get_white_list_plane_codes(self, row_flights: list) -> list:
+        white_list_codes_with_names: dict[str, str] = self.get_data()
+        white_list_planes: list = []
+
+        for row_flight in row_flights:
+            try:
+                code: str = row_flight['flight']['aircraft']['model']['code']
+            except TypeError:
+                code = 'Unknown'
+
+            if code in white_list_codes_with_names.keys():
+                white_list_planes.append(row_flight)
+
+        return white_list_planes
 
 
-def get_aircraft_name_by_code(code: str) -> str:
-    return get_json_config()[code]
+class AirlinesUtils(metaclass=Singleton):
 
+    __data: dict[str, str]
 
-def get_white_list_plane_codes(row_flights: list) -> list:
-    white_list_codes_with_names: dict[str, str] = get_json_config()
-    white_list_planes: list = []
+    def __init__(self):
+        self.__data = self.__load_json_data()
 
-    for row_flight in row_flights:
-        try:
-            code: str = row_flight['flight']['aircraft']['model']['code']
-        except TypeError:
-            code = 'Unknown'
+    def __load_json_data(self) -> dict[str, str]:
+        with open(os.getcwd() + '/config/airlines.json', 'r') as config_file:
+            string_json = config_file.read()
 
-        if code in white_list_codes_with_names.keys():
-            white_list_planes.append(row_flight)
+        return json.loads(string_json)
 
-    return white_list_planes
+    def get_data(self):
+        return self.__data
+
+    def get_airline_by_icao(self, icao: str) -> str:
+        if icao.upper() not in self.__data:
+            return 'Unknown'
+
+        return self.__data[icao]
 
 
 def convert_feet_to_meters(feets: int) -> int:
